08115ff25751641c9e38909243af011d147aa038
Tristan Pemble <tpemble@gmail.com>
2022-07-13 21:52:41 -0700

    fix: correct the option types

--- modules/lib.nix
+++ modules/lib.nix
@@ -4,12 +4,12 @@ with lib;
 
 {
   options._module.types = mkOption {
-    type = with types; raw;
+    type = with types; attrsOf optionType;
     visible = false;
     internal = true;
   };
   options._module.transformers = mkOption {
-    type = with types; raw;
+    type = with types; attrsOf raw;
     visible = false;
     internal = true;
   };
