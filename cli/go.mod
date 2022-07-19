b0d3df904b23c5816e78f55838af744ca8bc21fa
Tristan Pemble <tpemble@gmail.com>
2022-07-18 20:44:10 -0700

    wip

--- cli/go.mod
+++ cli/go.mod
@@ -12,9 +12,11 @@ require (
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/nomad/api v0.0.0-20220718210045-a7025e6ca476 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/rs/zerolog v1.27.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- cli/main.go
+++ cli/main.go
@@ -1,68 +1,95 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gosuri/uilive"
 	"github.com/hashicorp/nomad/api"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"math/rand"
+	"os"
+	"sort"
 	"time"
 )
 
 func main() {
 	const numJobs = 5
 
-	jobs := make(chan *DispatchJob, numJobs)
-	results := make(chan error, numJobs)
-
-	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+	jobNames := []string{
+		"lb",
+		"web",
+		"api",
+		"db",
+		"cache",
+		"search",
 	}
 
-	writer := uilive.New()
-	writer.Start()
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	for i := 0; i < 10; i++ {
-		jobs <- &DispatchJob{
-			id:     i,
-			status: "waiting",
-			job:    api.Job{},
-		}
+	ctx := context.Background()
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create Nomad API client")
+	}
+
+	stream, err := client.EventStream().Stream(ctx, map[api.Topic][]string{api.TopicEvaluation: jobNames}, 0, &api.QueryOptions{})
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create event stream")
+	}
+
+	jobs := make(chan *api.Job, len(jobNames))
+	results := make(chan *api.JobRegisterResponse, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(client, w, jobs, results)
+	}
+
+	messages := make(map[string]string)
+	for _, name := range jobNames {
+		messages[name] = "submitting"
+		jobs <- MakeJob(name)
 	}
 	close(jobs)
 
 	for a := 1; a <= numJobs; a++ {
 		<-results
 	}
+
+	w := uilive.New()
+	w.Start()
+	for events := range stream {
+		for _, event := range events.Events {
+			e, _ := event.Evaluation()
+			messages[e.JobID] = event.Type
+			Render(w, messages)
+			time.Sleep(1*time.Second + 3*time.Second*time.Duration(rand.Float32()))
+		}
+	}
+	w.Stop()
 }
 
-func worker(id int, jobs <-chan *DispatchJob, result chan<- error) {
-	for j := range jobs {
-		fmt.Println("worker", id, "started  job")
-		j.Dispatch()
-		fmt.Println("worker", id, "finished job")
-		result <- nil
+func Render(w *uilive.Writer, messages map[string]string) {
+	keys := make([]string, 0)
+	for key, _ := range messages {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, jobId := range keys {
+		fmt.Fprintf(w.Newline(), "[%s] %s\n", jobId, messages[jobId])
 	}
 }
 
-type DispatchJob struct {
-	id     int
-	status string
-	job    api.Job
+func MakeJob(name string) *api.Job {
+	task := api.NewTask("web", "exec")
+	group := api.NewTaskGroup("web", 1).AddTask(task)
+	return api.NewServiceJob(name, name, "global", 50).AddDatacenter("dc1").AddTaskGroup(group)
 }
 
-func (j *DispatchJob) Dispatch() {
-	<-time.After(1*time.Second + 5*time.Second*time.Duration(rand.Float32()))
-	//c, err := RenderConfigs(jobs)
-	//for _, id := range jobs {
-	//	job, err := ReadJobFile(id)
-	//	request, err := BuildApplyRequest(job)
-	//	response, err := SendRequest(request)
-	//	evaluation, err := ParseResponse(response)
-	//	for {
-	//		status, err := CheckEvaluation(evaluation)
-	//		if status == "done" {
-	//			break
-	//		}
-	//	}
-	//}
+func worker(client *api.Client, id int, jobs <-chan *api.Job, result chan *api.JobRegisterResponse) {
+	for job := range jobs {
+		res, _, _ := client.Jobs().Register(job, &api.WriteOptions{})
+		result <- res
+	}
 }
