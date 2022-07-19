043ae948a2b45a05e54718c9baf95e974953cb48
Tristan Pemble <tpemble@gmail.com>
2022-07-18 19:00:47 -0700

    wip

--- cli/go.mod
+++ cli/go.mod
@@ -5,9 +5,16 @@ go 1.17
 require github.com/spf13/cobra v1.5.0
 
 require (
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/hashicorp/cronexpr v1.1.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/nomad/api v0.0.0-20220718210045-a7025e6ca476 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- cli/main.go
+++ cli/main.go
@@ -3,83 +3,66 @@ package main
 import (
 	"fmt"
 	"github.com/gosuri/uilive"
+	"github.com/hashicorp/nomad/api"
 	"math/rand"
-	"sync"
 	"time"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
+	const numJobs = 5
+
+	jobs := make(chan *DispatchJob, numJobs)
+	results := make(chan error, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
 
 	writer := uilive.New()
 	writer.Start()
 
-	var progress []int
 	for i := 0; i < 10; i++ {
-		i := i
-		progress = append(progress, 0)
-
-		p := make(chan int, 1)
-
-		wg.Add(1)
-
-		// Start download
-		go download(p, wg)
-
-		// Track progress
-		go func() {
-			select {
-			case p := <-p:
-				progress[i] = p
-				writeProgress(progress, writer)
-			}
-		}()
-
-	}
-	wg.Wait()
-	writer.Stop()
-}
-
-func writeProgress(progress []int, writer *uilive.Writer) {
-	for i, p := range progress {
-		_, _ = fmt.Fprintf(writer.Newline(), "[%d] progress %d\n", i, p)
-	}
-	writer.Flush()
-}
-
-func worker(id int, jobs <-chan int, result chan<- int) {
-	for j := range jobs {
-		fmt.Println("worker", id, "started  job", j)
-		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
-		results <- j * 2
-	}
-}
-
-func download(p chan int, wg *sync.WaitGroup) {
-	for j := 0; j <= 100; j++ {
-		p <- j
-		<-time.After(time.Millisecond * time.Duration(10*rand.Float32()))
-	}
-	wg.Done()
-}
-
-type WorkerLog struct {
-	buffer []string
-}
-
-func DoTheDeploy(jobs []int) err {
-	c, err := RenderConfigs(jobs)
-	for _, id := range jobs {
-		job, err := ReadJobFile(id)
-		request, err := BuildApplyRequest(job)
-		response, err := SendRequest(request)
-		evaluation, err := ParseResponse(response)
-		for {
-			status, err := CheckEvaluation(evaluation)
-			if status == "done" {
-				break
-			}
+		jobs <- &DispatchJob{
+			id:     i,
+			status: "waiting",
+			job:    api.Job{},
 		}
 	}
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		<-results
+	}
+}
+
+func worker(id int, jobs <-chan *DispatchJob, result chan<- error) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job")
+		j.Dispatch()
+		fmt.Println("worker", id, "finished job")
+		result <- nil
+	}
+}
+
+type DispatchJob struct {
+	id     int
+	status string
+	job    api.Job
+}
+
+func (j *DispatchJob) Dispatch() {
+	<-time.After(1*time.Second + 5*time.Second*time.Duration(rand.Float32()))
+	//c, err := RenderConfigs(jobs)
+	//for _, id := range jobs {
+	//	job, err := ReadJobFile(id)
+	//	request, err := BuildApplyRequest(job)
+	//	response, err := SendRequest(request)
+	//	evaluation, err := ParseResponse(response)
+	//	for {
+	//		status, err := CheckEvaluation(evaluation)
+	//		if status == "done" {
+	//			break
+	//		}
+	//	}
+	//}
 }
