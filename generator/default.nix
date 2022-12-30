1a240c42af2f27402b81cfec01cdc50bf4931f37
Tomek Mańko <tomek.manko@railgun-solutions.com>
2022-12-30 18:33:29 +0100

    Update dependencies

--- generator/default.nix
+++ generator/default.nix
@@ -1,8 +1,9 @@
-{ buildGoApplication }:
+{ buildGoApplication, go_1_19 }:
 
 buildGoApplication {
   pname = "nix-nomad";
   version = "0.1";
   src = ./.;
+  go = go_1_19;
   modules = ./gomod2nix.toml;
 }
